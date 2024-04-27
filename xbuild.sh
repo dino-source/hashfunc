fe4900e07688cf43e6bdcf74e9145d3abd87c1b4
Dino Source <dinosource.cpp@gmail.com>
2024-04-27 16:41:02 -0700

    Refactor x-configs: get rid of $DEVELOP env variable

--- xbuild.sh
+++ xbuild.sh
@@ -1,4 +1,1 @@
-# You have to define DEVELOP variable as a path to your development 
-# directory in your .bashrc (or .zshrc if you use zsh) file
-
-/usr/bin/cmake --build $DEVELOP/hashfunc/build --config Debug --target all --
+/usr/bin/cmake --build ${PWD}/build --config Debug --target all --
--- xtests.sh
+++ xtests.sh
@@ -1,4 +1,1 @@
-# You have to define DEVELOP variable as a path to your development 
-# directory in your .bashrc (or .zshrc if you use zsh) file
-
-$DEVELOP/hashfunc/build/test/hashfunc_test
+${PWD}/build/test/`basename "${PWD}"`_test
